4de7a4a30e77d3e1bdaa0f81b93f93aefc27d458
Adam Burmister <adam.burmister@xero.com>
2014-04-09 16:54:01 -0700

    getmac

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var getmac = require('getmac');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  // Fetch the computer's mac address
+  require('getmac').getMac(function(err, macAddress){
+    if (err) {
+      throw err;
+    }
+    res.render('index', { title: macAddress });
+  });
 });
 
 module.exports = router;
