56bb6ea17e9131ddb558034c0df500436635333d
Adam Burmister <adam.burmister@xero.com>
2014-04-11 10:27:32 -0700

    Get correct MAC address

--- app/controllers/join.js
+++ app/controllers/join.js
@@ -25,39 +25,35 @@ exports.index = function(req, res) {
 }
 
 exports.verify = function(req, res) {
-  res.render('join/verify', { title: 'Register' });
+  require('getmac').getMac(function(err, macAddress){
+    res.render('join/verify', { title: 'Register', macAddress: macAddress });
+  });
 }
 
 exports.verifyCode = function(req, res) {
   var inviteCode = req.param('inviteCode');
+  var macAddress = req.param('macAddress');
+  console.log('Verifying invite code', inviteCode);
 
-  require('getmac').getMac(function(err, macAddress){
-    if (err) {
-      throw err; // There was no MAC address. That means it wasn't a LAN 
+  Employee.findOne({ inviteCode: +inviteCode }, function (err, employee) {
+    if (err) throw(err);
+    
+    if(employee) {
+      console.log('Invite code found', employee);
+
+      employee.inviteCode = null;
+      employee.macAddress = macAddress;
+      employee.save();
+
+      // Verified employees can access the internet as normal
+      exec("./allow-access.sh " + macAddress, function puts(error, stdout, stderr) {
+        sys.puts(stdout)
+        res.send({ success: true });
+      });
+    } else {
+      console.log('Invite code not found');
+      res.send({ success: false });
     }
-
-    console.log('Verifying invite code', inviteCode);
-
-    Employee.findOne({ inviteCode: +inviteCode }, function (err, employee) {
-      if (err) throw(err);
-      
-      if(employee) {
-        console.log('Invite code found', employee);
-
-        employee.inviteCode = null;
-        employee.macAddress = macAddress;
-        employee.save();
-
-        // Verified employees can access the internet as normal
-        exec("./allow-access.sh " + macAddress, function puts(error, stdout, stderr) {
-          sys.puts(stdout)
-          res.send({ success: true });
-        });
-      } else {
-        console.log('Invite code not found');
-        res.send({ success: false });
-      }
-    });
   });
 
 } 
