f3f72e2b8fecee4ac57a6130234be6f38ad439d9
Adam Burmister <adam.burmister@xero.com>
2014-04-11 13:29:55 -0700

    lease path

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,4 @@
+var dhcpLease = require('../lib/dhcpLease')
 var mongoose = require('mongoose');
 var Employee = mongoose.model('Employee');
 
@@ -7,16 +8,25 @@ exports.index = function(req, res) {
      req.socket.remoteAddress ||
      req.connection.socket.remoteAddress;
 
-  Employee.findOne({ ipAddress: ipAddress }, function (err, employee) {
-    if (err) throw(err);
+  dhcpLease.getMac(ipAddress, function(err, macAddress) {
     
-    if(!employee) {
-      // We don't know this user. Redirect to joining page
-      res.redirect('/join');
+    if(!macAddress) {
+      console.log(err);
+      res.send("No MAC address was found for your IP address");
     } else {
-      // We know this user. Redirect to timesheet
-      res.redirect('/timesheet');
+      Employee.findOne({ ipAddress: ipAddress }, function (err, employee) {
+        if (err) throw(err);
+        
+        if(!employee) {
+          // We don't know this user. Redirect to joining page
+          res.redirect('/join');
+        } else {
+          // We know this user. Redirect to timesheet
+          res.redirect('/timesheet');
+        }
+      });
     }
+
   });
 
 }
---
+++ app/lib/dhcpLease.js
@@ -0,0 +1,29 @@
+// Load the .env file with our config
+var dotenv = require('dotenv');
+var envPath = __dirname + '/../.env';
+dotenv._getKeysAndValuesFromEnvFilePath(envPath);
+dotenv._setEnvs();
+
+var fs = require('fs');
+
+var leasePath = process.env.DHCP_LEASE_FILE_PATH || "/var/lib/dhcp/dhcpd.leases";
+var macRegex = /(?:[a-z0-9]{2}[:\-]){5}[a-z0-9]{2}/ig;
+
+exports.getMac = function(ipAddress, next) {
+  console.log("Looking up MAC for", ipAddress, 'in', leasePath);
+  var data = fs.readFile(leasePath, function(err, data) {
+    if(err) {
+      throw err;
+    }
+
+    var content = data.toString();
+    var leaseRegex = new RegExp("lease " + ipAddress + " \{.*hardware ethernet ((?:[a-z0-9]{2}[:\-]){5}[a-z0-9]{2}).*\}", "i");
+
+    var matches = content.match(leaseRegex);
+    if(!matches) {
+      next("MAC not found", null);
+    } else {
+      next(null, matches[1]);
+    }
+  })
+}
